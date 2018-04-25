79f23d005940d17536c50673c02190333ba3d01a
Supreet Shetty <shetty.supreet@gmail.com>
2018-04-26 02:41:19 +0530

    Initial commit

--- models/model.py
+++ models/model.py
@@ -1,21 +1,13 @@
-import MySQLdb
-from sqlalchemy import create_engine
-from sqlalchemy import event
-from sqlalchemy.engine import Engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.pool import NullPool
+import json
 
+from connectors import mysql
 import config
+from oto import response
 
-BaseModel = declarative_base()
-_db_engine = create_engine(config.db_url, poolclass=NullPool)
-_db_session = sessionmaker(bind=_db_engine)
 
 def get_call_data():
 	try:
-		connection = MySQLdb.connect(**config.db_config)
-		cursor = connection.cursor(MySQLdb.cursors.DictCursor)
+		connection, cursor = mysql.connection_cursor()
 		q = ("SELECT * FROM test")
 		cursor.execute(q)
 		rows = cursor.fetchall()
@@ -23,4 +15,4 @@ def get_call_data():
 	except:
 			rows = {}
 
-	return rows
+	return json.dumps(rows)

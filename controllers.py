f9621fe47db6e04bd3844f58df17031113971f17
Supreet Shetty <shetty.supreet@gmail.com>
2018-04-26 02:41:19 +0530

    Initial commit

--- controllers.py
+++ controllers.py
@@ -2,6 +2,7 @@ import config
 import json
 import MySQLdb
 
+import logic
 from sqlalchemy import create_engine
 from sqlalchemy import event
 from sqlalchemy.engine import Engine
@@ -20,16 +21,7 @@ _db_session = sessionmaker(bind=_db_engine)
 class Controller:
     def on_get(self, req, resp):
         """Handles GET requests"""
-        try:
-            connection = MySQLdb.connect(**config.db_config)
-            cursor = connection.cursor(MySQLdb.cursors.DictCursor)
-            q = ("SELECT * FROM test")
-            cursor.execute(q)
-            rows = cursor.fetchall()
-            resp.body = json.dumps(rows)
-            connection.close()
-        except:
-            raise
+        rows = logic.get_call()
         resp.body = json.dumps(rows)
 
     def on_post(self, req, resp):
